bd748080ca17f47169c5b91f0bb5cf03e914cf79
chinelo.okoli <chinelo.okoli@gmail.com>
2020-06-24 15:09:01 +0100

    Implement discount() method for bogof on ItemByUnit - TDD test case passes

--- src/main/java/kata/supermarket/ItemByUnit.java
+++ src/main/java/kata/supermarket/ItemByUnit.java
@@ -1,6 +1,7 @@
 package kata.supermarket;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 public class ItemByUnit implements DiscountItem {
 
@@ -23,7 +24,20 @@ public class ItemByUnit implements DiscountItem {
 
 	@Override
 	public BigDecimal discount() {
-		// TODO implement bogof discounting for items by unit
-		return BigDecimal.ZERO;
+		final BigDecimal TWO = BigDecimal.valueOf(2);
+		
+		// is units even ?
+		boolean isEven = this.units.remainder(TWO).equals(BigDecimal.ZERO);
+		
+		BigDecimal discount;
+		
+		// implement bogof discount by halving price for multiple even items
+		// when unit is single product, there is no discount
+		// when unit is multiple odd count then half off the even items only
+		if (isEven) discount = this.price().divide(TWO).setScale(2, RoundingMode.HALF_UP);
+		else if (this.units.equals(BigDecimal.ONE)) discount = BigDecimal.ZERO; // no discount on single unit
+		else discount = this.price().subtract(this.product.pricePerUnit()).divide(TWO).setScale(2, RoundingMode.HALF_UP);
+		
+		return discount;
 	}
 }
